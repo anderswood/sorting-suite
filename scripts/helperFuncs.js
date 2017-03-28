a638921a7d8a464c856d1dbdc943d70d81981d59
Anders Wood <anderswood@gmail.com>
2017-03-28 17:02:43 -0600

    complete merge sort - add returngit add .

--- scripts/helperFuncs.js
+++ scripts/helperFuncs.js
@@ -1,10 +1,15 @@
 
 const randNumGen = (min, max, n) => {
   let randomArray = [];
+
   for (let i = 0; i < n; i++) {
     randomArray.push(Math.floor(Math.random() * (max - min)) + min);
   }
   return randomArray;
 }
 
-export default randNumGen;
+const compareNumbers = (a, b) => {
+  return a - b;
+}
+
+export {randNumGen, compareNumbers};
